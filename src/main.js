4cb71f41f5db44b7bc7485c058c8134357110e83
Evhen Kulbachenko <45nalitay45@gmail.com>
2024-08-22 10:50:28 +0300

    check3

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ const errorMessege = str =>
     position: 'topRight',
     backgroundColor: '#EF4040',
     messageColor: '#FAFAFB',
-    icongi,
+    iconUrl: './img/error.svg',
     messageSize: '16px',
     messageLineHeight: '24px',
     maxWidth: '432px',
